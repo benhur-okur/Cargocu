27dfde8cfa6dff834ef5d858b1663c8f2c7ef0ad
Doğa Güneş <120034878+doagunes@users.noreply.github.com>
2026-05-29 18:23:16 +0300

    added animations and sounds

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -10,46 +10,82 @@ public class Collision : MonoBehaviour
     [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
 
     SpriteRenderer spriteRenderer;
+
     [SerializeField] Text packageText;
     [SerializeField] DeliveryManager deliveryManager;
     [SerializeField] ScoreManager scoreManager;
 
+    [SerializeField] AudioClip pickupSound;
+    [SerializeField] AudioClip deliverySound;
+    [SerializeField] GameObject deliveryEffectPrefab;
+
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
         if (packageText != null)
+        {
             packageText.text = "Kargo Yok";
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package" && !hasPacked)
+        if (other.CompareTag("Package") && !hasPacked)
         {
+            if (pickupSound != null)
+            {
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+            }
+
             hasPacked = true;
             hasDelivered = false;
+
             spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject);
-            if (packageText != null)
-                packageText.text = "Kargo Var!";
 
-            // kargo alýnýnca delivery point aç
+            if (packageText != null)
+            {
+                packageText.text = "Kargo Var!";
+            }
+
             if (deliveryManager != null)
+            {
                 deliveryManager.SelectNewDelivery();
+            }
         }
-        else if (other.tag == "Customer" && hasPacked && !hasDelivered)
+        else if (other.CompareTag("Customer") && hasPacked && !hasDelivered)
         {
+            Debug.Log("TESLIMAT CALISTI");
             hasPacked = false;
             hasDelivered = true;
+
+            if (deliverySound != null)
+            {
+                AudioSource.PlayClipAtPoint(deliverySound, transform.position);
+            }
+
+            if (deliveryEffectPrefab != null)
+            {
+                Instantiate(deliveryEffectPrefab, other.transform.position, Quaternion.identity);
+            }
+
             spriteRenderer.color = noPackageColor;
+
             if (packageText != null)
+            {
                 packageText.text = "Kargo Yok";
+            }
 
             if (scoreManager != null)
+            {
                 scoreManager.AddScore(10);
+            }
 
-            // teslim edince delivery point kapat
             if (deliveryManager != null)
+            {
                 deliveryManager.HideDelivery();
+            }
         }
     }
 }
